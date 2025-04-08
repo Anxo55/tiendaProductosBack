6bfc7d32d66490d78430df1338ee80705f583984
Anxo55 <anxo.campos260204@gmail.com>
2025-04-08 08:25:01 +0200

    first-commit

---
+++ src/main/java/cebem/tiendaProductos/TiendaProductosApplication.java
@@ -0,0 +1,13 @@
+package cebem.tiendaProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaProductosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/cebem/tiendaProductos/entities/User.java
@@ -0,0 +1,44 @@
+package cebem.tiendaProductos.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles")
+    private Set<String> roles = new HashSet<>();
+    
+}
